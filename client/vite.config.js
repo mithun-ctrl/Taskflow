c0e7407e9e7c63eb18a54608adf302d66c4c1492
mithun-ctrl <itsmithun01@gmail.com>
2026-05-21 23:34:31 +0530

    base url changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://taskflow-assesment.up.railway.app',
         changeOrigin: true,
       },
     },
